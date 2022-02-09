35a2d2020e926d9ffe4b19de9d4a90ecb3a23bb8
mjjrt <64268845+mjjrt@users.noreply.github.com>
2022-02-09 18:57:35 +0100

    Setup and Initial Commit

---
+++ src/main.cpp
@@ -0,0 +1,99 @@
+#include <Arduino.h>
+
+#ifndef __WIRE_H
+#include <Wire.h>
+#endif
+
+#include <LiquidCrystal.h>
+
+#define AnalogPin A0
+
+#define TimeStep 1000 // Update bounds every 1000 ms
+
+// Limit declarations; these get updated in SweepLow() and SweepHigh() Functions
+#define MID_RANGE 512
+int lowerlimit = 250;
+int upperlimit = 750;
+
+int highs = 0;
+int lows = 0;
+
+// Initialisation for LCD
+const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+
+// Function declarations
+int ReadOut();
+void Diffs();
+void SetLimits();
+
+///
+void setup() {
+  Serial.begin(9600);
+
+  lcd.begin(16, 2);
+  //lcd.print("hello, world!");
+}
+
+void loop()
+{
+  uint32_t start = millis();
+
+  SetLimits();
+  do {
+    Diffs();
+  } while(millis() - start < TimeStep); // loops every 1 second
+
+  int bpm = (lows + highs)/2 * 60/TimeStep;
+  lcd.print(bpm);
+}
+
+///
+int ReadOut()
+{
+  int sig = analogRead(AnalogPin);
+  return sig;
+}
+
+
+int SweepLow()
+{
+  for (int i = MID_RANGE; i > 0; i--)
+  {
+    if (i > ReadOut())
+    {
+      return i; // Return i as low bound
+      break;
+    }
+  }
+  return 0;
+}
+
+int SweepHigh()
+{
+  for(int i = MID_RANGE; i < 1023; i++)
+  {
+    if(i > ReadOut())
+    {
+      return i;
+      break;
+    }
+  }
+  return 0;
+}
+
+void SetLimits()
+{
+  lowerlimit = SweepLow();
+  upperlimit = SweepHigh();
+}
+
+void Diffs()
+{
+  if (ReadOut() < lowerlimit)
+    lows++;
+
+  if (ReadOut() > upperlimit)
+    highs++;
+}
+
